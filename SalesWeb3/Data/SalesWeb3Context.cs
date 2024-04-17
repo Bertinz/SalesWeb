5456f8d86f7827e9737fa2e7586eabacc4420fc3
Bertinz <humbertozanqueta@gmail.com>
2024-04-17 10:41:51 -0300

    CRUD Scaffolding

---
+++ SalesWeb3/Data/SalesWeb3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWeb3.Models;
+
+namespace SalesWeb3.Data
+{
+    public class SalesWeb3Context : DbContext
+    {
+        public SalesWeb3Context (DbContextOptions<SalesWeb3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWeb3.Models.Department> Department { get; set; }
+    }
+}
